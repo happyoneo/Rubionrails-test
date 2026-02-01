0c0f0fedf3a68ea1ec6d057a274636bf1ff3aa64
tomcat <12403953@iscu.ac.kr>
2026-02-01 13:48:47 +0900

    Update routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   root "home#index"
+  
+  # Home page
   get "home/index"
+  
+  # Services page
+  get "services/index", as: "services"
+  
+  # Staff page
+  get "staff/index", as: "staff"
+  
+  # Location page
+  get "location/index", as: "location"
+  
+  # Booking page
+  get "booking/index", as: "booking"
 end
