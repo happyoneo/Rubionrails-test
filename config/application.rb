50228403b3a0289b0cdecf2780d1a78f1c71d5d7
tomcat <12403953@iscu.ac.kr>
2026-02-01 15:10:39 +0900

    Update config.load_defaults from 8.0 to 7.1 in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,4 @@
-require_relative "boot"
-require "rails/all"
-
-module VeterinaryHospital
-  class Application < Rails::Application
-    config.load_defaults 8.0
-  end
+# Load defaults for Rails version
+Rails.application.configure do
+  config.load_defaults 7.1
 end
